63afef57398623bd5a06e7fdaeaf319af1f26916
Jivago Alves <jivagoalves@gmail.com>
2013-07-24 14:55:47 -0300

    Use mysql instead of mariadb for Fedora >= 19

--- my-fedora-laptop.pp
+++ my-fedora-laptop.pp
@@ -28,9 +28,24 @@ include postgresqllibs
 
 # MySQL stuff
 # From puppetlabs/mysql
-class { 'mysql': }
-class { 'mysql::ruby': }
-class { 'mysql::server': config_hash => { 'root_password' => '' } }
+if $::operatingsystem == 'Fedora' and $::operatinsystemrelease >= 19 {
+  # Declare the packages explicitly,
+  # otherwise this will install mariadb for Fedora >= 19
+  class { 'mysql':
+    client_package_name => 'community-mysql',
+    server_package_name => 'community-mysql-server'
+  }
+  class { 'mysql::ruby': }
+  class { 'mysql::server': config_hash => { 'root_password' => 'mysql' } }
+  # Also install developement package
+  package { 'community-mysql-devel': }
+} else {
+  class { 'mysql': }
+  class { 'mysql::ruby': }
+  class { 'mysql::server': config_hash => { 'root_password' => 'mysql' } }
+  # Also install developement package
+  package { 'mysql-devel': }
+}
 
 include nokogiri
 
