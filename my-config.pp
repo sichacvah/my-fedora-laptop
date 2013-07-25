40d4a0529a351965045bc89c6613725e8e435a30
Jivago Alves <jivagoalves@gmail.com>
2013-07-25 00:26:26 -0300

    Link .gitconfig to dotfiles/gitconfig

--- my-config.pp
+++ my-config.pp
@@ -8,6 +8,14 @@ vcsrepo { "${codepath}/dotfiles":
   source   => 'https://github.com/jivagoalves/dotfiles.git'
 }
 
+# .gitconfig from my dotfiles repo
+file { "${homedir}/.gitconfig":
+  ensure  => link,
+  target  => "${codepath}/dotfiles/gitconfig",
+  require => Vcsrepo["${codepath}/dotfiles"]
+}
+
+
 # vim-pathogen
 include vim
 
@@ -17,7 +25,6 @@ file { "${vim::vimrc}":
   target  => "${codepath}/dotfiles/vimrc",
   require => Vcsrepo["${codepath}/dotfiles"]
 }
-
 
 # My vim's plugins
 vim::bundle { [
