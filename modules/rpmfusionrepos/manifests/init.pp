94d3545a8ef469df60449e86f63fe10283d030ab
Jivago Alves <jivagoalves@gmail.com>
2013-07-27 18:27:27 -0300

    Pass `-y` to yum and fix the urls for the packages

--- modules/rpmfusionrepos/manifests/init.pp
+++ modules/rpmfusionrepos/manifests/init.pp
@@ -2,16 +2,16 @@ class rpmfusionrepos {
   $yum = "/usr/bin/yum"
   $rpm = "/usr/bin/rpm"
 
-  $rpmfusionfree    = "rpmfusion-free-release-$(${rpm} -E %fedora).noarch.rpm"
-  $rpmfusionnonfree = "rpmfusion-nonfree-release-$(${rpm} -E %fedora).noarch.rpm"
+  $rpmfusionfree    = "rpmfusion-free-release-$(${rpm} -E %fedora).noarch"
+  $rpmfusionnonfree = "rpmfusion-nonfree-release-$(${rpm} -E %fedora).noarch"
 
   exec { "${rpmfusionfree}":
-    command => "${yum} localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/${rpmfusionfree}",
+    command => "${yum} localinstall -y --nogpgcheck http://download1.rpmfusion.org/free/fedora/${rpmfusionfree}.rpm",
     unless  => "${rpm} -q ${rpmfusionfree}"
   }
 
   exec { "${rpmfusionnonfree}":
-    command => "${yum} localinstall --nogpgcheck http://download1.rpmfusion.org/free/fedora/${rpmfusionnonfree}",
+    command => "${yum} localinstall -y --nogpgcheck http://download1.rpmfusion.org/nonfree/fedora/${rpmfusionnonfree}.rpm",
     unless  => "${rpm} -q ${rpmfusionnonfree}"
   }
 }
